21130172504ff1f2992d97dcc94ddcb5189915c0
Natania Rayos <aniarayos@gmail.com>
2021-11-25 13:32:45 +0800

    Added Reservation Class

---
+++ ChurchSched/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChurchNameSpace
+{
+    class Reservation
+    {
+        private long id;
+        string type, name, contactNumber, emailAddress, homeAddress;
+        DateTime date, time;
+
+        public void confirmReservation()
+        {
+
+        }
+        public void editReservation()
+        {
+
+        }
+        public void cancelReservation()
+        {
+
+        }
+
+        public long newID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string newType
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+        public string newName
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string newContact
+        {
+            get { return this.contactNumber; }
+            set { this.contactNumber = value; }
+        }
+        public string newEmail
+        {
+            get { return this.emailAddress; }
+            set { this.emailAddress = value; }
+        }
+        public string newAddress
+        {
+            get { return this.homeAddress; }
+            set { this.homeAddress = value; }
+        }
+        public DateTime newDate
+        {
+            get { return this.date; }
+            set { this.date = value; }
+        }
+        public DateTime newTime
+        {
+            get { return this.time; }
+            set { this.time = value; }
+        }
+    }
+}
