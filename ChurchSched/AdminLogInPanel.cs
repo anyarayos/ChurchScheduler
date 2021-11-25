2ba8d19c8c91fd3fe5e133cd1ac3c3caa3be5003
Lopez, Percival IV <lopez.078934@globalcity.sti.edu.ph>
2021-11-25 18:06:27 +0800

    login form sqlite functionality

--- ChurchSched/AdminLogInPanel.cs
+++ ChurchSched/AdminLogInPanel.cs
@@ -8,32 +8,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data;
-using System.Data.SqlClient;
+using System.Data.SQLite;
 
 
 namespace ChurchSched
 {
     public partial class frmLogIn : Form
     {
-        private string username, password;
+        // sql variables and objects
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        // set connection method
+        private void SetConnection()
+								{
+            sql_con = new SQLiteConnection("Data Source=Church.db; Version=3; New=False; Compress=True;");
+								}
+
+        // execute query NOT SURE IF IMPORTANT
+        private void ExecuteQuery(string txtQuery)
+								{
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+								}
+
+        // variables
         private int attempt = 0;
         
-        public frmLogIn()
-        {
-            InitializeComponent();
-        }
-
-      
-
+        // 
         private void btnLogIn_Click(object sender, EventArgs e)
         {
-            /*Remove the commnet enabler kung meron ng database hehehehe
-            //SqlConnection con = new SqlConnection("");//put the sql resources inside the ""
-            //SqlDataAdapter Sda = new SqlDataAdaptor("SELECT * FROM ---- WHERE username = '"+txtUserName.Text+"' and password = '"+txtPassword.Text+"'", con); //---- meaning the name of the table with the credential of the admins
-            //DataTable dt = new DataTable();
-            //sda.Fill(dt);
-            if(dt.row.Count > 0)
+            // sql connection 
+            SetConnection();
+            
+            // Data Table
+            DB = new SQLiteDataAdapter("SELECT * FROM Accounts WHERE username = '" + txtUserName.Text + "' AND password = '" + txtPassword.Text + "'", sql_con);
+            DT = new DataTable();
+            DB.Fill(DT);
+
+            // verifyLogin
+            if (DT.Rows.Count == 1)
             { 
                 this.DialogResult = DialogResult.Yes;
                 this.Dispose();
@@ -52,32 +73,11 @@ namespace ChurchSched
                     this.Dispose();
                 }
             }
-            */
-            Administrator admin = new Administrator("Gelay Magalona", "GelayCEO", "password123");
+        }
 
-            username = txtUserName.Text;
-            password = txtPassword.Text;
-
-            if(admin.verifyLogin(username, password))
-            {
-                this.DialogResult = DialogResult.Yes;
-                this.Dispose();
-            }
-            else
-            {
-                attempt++;
-                MessageBox.Show("Incorrect Username or Password. \nPlease try again: \nAttempts: " + attempt);
-                txtUserName.Text = "";
-                txtPassword.Text = "";
-
-                if (attempt == 5)
-                {
-                    MessageBox.Show("Maximum attempts is reach. \nApplication will now exit");
-                    this.DialogResult = DialogResult.No;
-                    this.Dispose();
-                }
-            }
-            
+        public frmLogIn()
+        {
+            InitializeComponent();
         }
 
         private void frmLogIn_Load(object sender, EventArgs e)
@@ -90,7 +90,6 @@ namespace ChurchSched
             ForgotPassSendCode fpsc = new ForgotPassSendCode();
             this.Hide();
             fpsc.Show();
-
         }
     }
 }
